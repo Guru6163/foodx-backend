22c8529fa20e9737d56c9d82668719e19c217b82
Guruprasath <guruprasath@jarvisml.com>
2023-07-17 22:06:30 +0530

    Port Change

--- server.js
+++ server.js
@@ -6,10 +6,12 @@ const cors = require('cors');
 
 dotenv.config({ path: './.env' })
 
-app.use(cors({
-  origin: '*',
-  allowHeaders: ['Content-Type'],
-}));
+app.use(
+  cors({
+    origin: 'http://127.0.0.1:5173',
+  })
+);
+
 
 const db = process.env.DATABASE_URL
 
