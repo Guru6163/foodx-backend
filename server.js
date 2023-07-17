ca7e34e5aa66f68e806a40b63b85c3603fdcdbb0
Guruprasath <guruprasath@jarvisml.com>
2023-07-17 22:16:03 +0530

    Cors Change

--- server.js
+++ server.js
@@ -1,26 +1,18 @@
-const app = require("./app")
-const dotenv = require("dotenv")
-const mongoose = require("mongoose")
+const app = require("./app");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 const cors = require('cors');
 
+dotenv.config({ path: './.env' });
 
-dotenv.config({ path: './.env' })
+app.use(cors());
 
-app.use(
-  cors({
-    origin: 'http://127.0.0.1:5173',
-  })
-);
-
-
-const db = process.env.DATABASE_URL
-
-const port = process.env.PORT
+const db = process.env.DATABASE_URL;
+const port = process.env.PORT;
 
 app.listen(port, () => {
-  console.log(`App running on Port: ${port}`)
-})
-
+  console.log(`App running on Port: ${port}`);
+});
 
 mongoose.connect(db, {
   useNewUrlParser: true,
@@ -32,5 +24,3 @@ mongoose.connect(db, {
   .catch((error) => {
     console.log("Failed to connect to MongoDB", error);
   });
-
-
